3457856d5c0328a8d27e11cc972db5154bfb0e3e
yashagozwan <yashagozwan13@gmail.com>
2022-04-26 23:28:50 +0700

    add take picture action

--- app/src/main/java/com/yashagozwan/mystorynew/ui/addstory/AddStoryActivity.kt
+++ app/src/main/java/com/yashagozwan/mystorynew/ui/addstory/AddStoryActivity.kt
@@ -3,10 +3,14 @@ package com.yashagozwan.mystorynew.ui.addstory
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -14,16 +18,30 @@ import com.yashagozwan.mystorynew.R
 import com.yashagozwan.mystorynew.databinding.ActivityAddStoryBinding
 import com.yashagozwan.mystorynew.ui.ViewModelFactory
 import com.yashagozwan.mystorynew.ui.camera.CameraActivity
+import com.yashagozwan.mystorynew.utils.Utils.rotateBitmap
+import java.io.File
 
 class AddStoryActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var binding: ActivityAddStoryBinding
     private val factory = ViewModelFactory.getInstance(this)
     private val addStoryViewModel: AddStoryViewModel by viewModels { factory }
 
+    private val launcherIntentCameraX = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == CAMERA_X_RESULT) {
+            val myFile = it.data?.getSerializableExtra("picture") as File
+            val isBackCamera = it.data?.getBooleanExtra("isBackCamera", true) as Boolean
+            val result = rotateBitmap(BitmapFactory.decodeFile(myFile.path), isBackCamera)
+            binding.ivImagePreview.setImageBitmap(result)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAddStoryBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setTitleAppBar()
 
         if (!allPermissionsGranted()) {
             ActivityCompat.requestPermissions(this, REQUIRE_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
@@ -32,20 +50,27 @@ class AddStoryActivity : AppCompatActivity(), View.OnClickListener {
         setButtonListener()
     }
 
+    private fun setTitleAppBar() {
+        supportActionBar?.title = getString(R.string.add_story)
+    }
+
     private fun setButtonListener() {
         binding.btnCamera.setOnClickListener(this)
+        binding.btnGallery.setOnClickListener(this)
+        binding.btnUpload.setOnClickListener(this)
     }
 
     override fun onClick(view: View) {
         when (view.id) {
             R.id.btn_camera -> startCameraX()
+            R.id.btn_gallery -> {}
+            R.id.btn_upload -> {}
         }
     }
 
     private fun startCameraX() {
-        Intent(this, CameraActivity::class.java).also {
-            startActivity(it)
-        }
+        val intent = Intent(this, CameraActivity::class.java)
+        launcherIntentCameraX.launch(intent)
     }
 
     private fun allPermissionsGranted() = REQUIRE_PERMISSIONS.all {
--- app/src/main/java/com/yashagozwan/mystorynew/ui/camera/CameraActivity.kt
+++ app/src/main/java/com/yashagozwan/mystorynew/ui/camera/CameraActivity.kt
@@ -1,5 +1,6 @@
 package com.yashagozwan.mystorynew.ui.camera
 
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -9,11 +10,14 @@ import android.view.WindowManager
 import android.widget.Toast
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
 import com.yashagozwan.mystorynew.R
 import com.yashagozwan.mystorynew.databinding.ActivityCameraBinding
+import com.yashagozwan.mystorynew.ui.addstory.AddStoryActivity
+import com.yashagozwan.mystorynew.utils.Utils.createFile
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
@@ -31,7 +35,6 @@ class CameraActivity : AppCompatActivity(), View.OnClickListener {
         setButtonListener()
     }
 
-
     private fun setButtonListener() {
         binding.ivCaptureImage.setOnClickListener(this)
         binding.ivSwitchCamera.setOnClickListener(this)
@@ -39,7 +42,7 @@ class CameraActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onClick(view: View) {
         when (view.id) {
-            R.id.iv_capture_image -> {}
+            R.id.iv_capture_image -> takePicture()
             R.id.iv_switch_camera -> switchCamera()
         }
     }
@@ -76,6 +79,32 @@ class CameraActivity : AppCompatActivity(), View.OnClickListener {
             }
 
         }, ContextCompat.getMainExecutor(this))
+    }
+
+    private fun takePicture() {
+        val imageCapture = imageCapture ?: return
+        val photoFile = createFile(application)
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+        imageCapture.takePicture(
+            outputOptions,
+            ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                    val intent = Intent()
+                    intent.putExtra("picture", photoFile)
+                    intent.putExtra(
+                        "isBackCamera",
+                        cameraSelector == CameraSelector.DEFAULT_BACK_CAMERA
+                    )
+                    setResult(AddStoryActivity.CAMERA_X_RESULT, intent)
+                    finish()
+                    showToast("Berhasil mengambil gambar")
+                }
+
+                override fun onError(exception: ImageCaptureException) {
+                    showToast("Gagal mengambil gambar")
+                }
+            })
     }
 
     private fun hideSystemUI() {
